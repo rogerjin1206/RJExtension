e431457dd5cf15d6253b3f5c1c4ff811e1e532fd
LEEJAEJIN <jinlee1206@naver.com>
2018-06-10 16:30:52 +0900

    Added Example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by ROGER on 2018. 6. 10..
+//  Copyright © 2018년 LEEJAEJIN. All rights reserved.
+//
+
+import UIKit
+import RJExtension
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(self.className)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Example/Pods/Target Support Files/RJExtension/RJExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RJExtension : NSObject
+@end
+@implementation PodsDummy_RJExtension
+@end
