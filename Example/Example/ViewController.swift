c26ac819109dd46eeab2ce87d662d73387908762
LEEJAEJIN <jinlee1206@naver.com>
2018-08-07 14:33:41 +0900

    no message

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print(self.className)
+        
+        
+  
     }
 
     override func didReceiveMemoryWarning() {
---
+++ RJExtension/UIAlertController.swift
@@ -0,0 +1,11 @@
+//
+//  UIAlertController.swift
+//  RJExtension
+//
+//  Created by ROGER on 06/08/2018.
+//  Copyright © 2018 LEEJAEJIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
